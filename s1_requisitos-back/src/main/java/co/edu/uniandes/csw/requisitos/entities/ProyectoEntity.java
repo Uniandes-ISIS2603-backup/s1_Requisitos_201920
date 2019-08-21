f284d7383fb07f650849be9817ee46def21d075f
jj.martinezc@uniiandes.edu.co <>
2019-08-21 08:59:34 -0500

    Se creó tabla ProyectoEntity

---
+++ s1_requisitos-back/src/main/java/co/edu/uniandes/csw/requisitos/entities/ProyectoEntity.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requisitos.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Juan Martinez
+ */
+@Entity
+public class ProyectoEntity extends BaseEntity implements Serializable{
+    
+    private String nombre;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaInicial;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaFinal;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the fechaInicial
+     */
+    public Date getFechaInicial() {
+        return fechaInicial;
+    }
+
+    /**
+     * @param fechaInicial the fechaInicial to set
+     */
+    public void setFechaInicial(Date fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    /**
+     * @return the fechaFinal
+     */
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    /**
+     * @param fechaFinal the fechaFinal to set
+     */
+    public void setFechaFinal(Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+    
+}
