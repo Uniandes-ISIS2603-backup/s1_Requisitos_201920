f93a00e2f6c83bbdb13cedeeaa1d99389054f2ed
Nicole Bahamon <n.bahamon@uniandes.edu.co>
2019-08-21 16:05:00 -0500

    Se crearon los atributos AtributoCalidadEntity y FuncionalEntity con sus respectivas clases de persistencia y Test

---
+++ s1_requisitos-back/src/main/java/co/edu/uniandes/csw/requisitos/persistence/FuncionalPersistence.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requisitos.persistence;
+
+import co.edu.uniandes.csw.requisitos.entities.FuncionalEntity;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Nicole Bahamon
+ */
+@Stateless
+public class FuncionalPersistence {
+    
+    public FuncionalEntity create(FuncionalEntity funcional){
+            throw new java.lang.UnsupportedOperationException("Not supported yet.");
+                    }
+    
+}
