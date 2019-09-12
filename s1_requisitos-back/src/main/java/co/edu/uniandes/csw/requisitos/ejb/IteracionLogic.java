a5da491a166ed924e32e9de95d8811af75534ad7
rodrigo0097 <>
2019-09-12 08:51:47 -0500

    logica de la iteracion

---
+++ s1_requisitos-back/src/main/java/co/edu/uniandes/csw/requisitos/ejb/IteracionLogic.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requisitos.ejb;
+
+import co.edu.uniandes.csw.requisitos.entities.IteracionEntity;
+import co.edu.uniandes.csw.requisitos.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.requisitos.persistence.IteracionPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Rodrigo José González
+ */
+public class IteracionLogic {
+     /**
+    * Clase persistence de iteracion.
+    */  
+    @Inject
+    private IteracionPersistence ip;
+    
+    /**
+   * Crea una iteracion asegurandose de cumplir todas las reglas logicas de la apliacion.
+   * @param iteracion. Requisito entity a ser verificado.
+   * @return
+   * @throws BusinessLogicException 
+   */
+  public IteracionEntity createIteracion(IteracionEntity iteracion) throws BusinessLogicException
+  {
+       if(iteracion.getFechaFin()==null||iteracion.getFechaInicio() == null)
+      {
+         throw new BusinessLogicException("No hay fechas definidas para el proyecto");      
+      }
+      iteracion=ip.create(iteracion);
+      return iteracion;
+  }
+  
+   /**
+     * Obtener todos los interaciones existentes en la base de datos.
+     * @return una lista de requisitos.
+     */
+    public List<IteracionEntity> getDesarrolladores() 
+    {
+        List<IteracionEntity> iteraciones = ip.findAll();
+        return iteraciones;
+    }
+    
+}
