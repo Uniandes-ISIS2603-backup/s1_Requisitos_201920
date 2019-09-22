0314f7194d512b6e6c4ddf512b6a2d44faf46a9f
JuanMartinez <jj.martinezc@uniandes.edu.co>
2019-09-22 16:51:26 -0500

    Logica EquipoDesarrollo implementado

---
+++ s1_requisitos-back/src/test/java/co/edu/uniandes/csw/requisitos/test/logic/EquipoDesarrolloLogicTest.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requisitos.test.logic;
+
+import co.edu.uniandes.csw.requisitos.ejb.EquipoDesarrolloLogic;
+import co.edu.uniandes.csw.requisitos.entities.EquipoDesarrolloEntity;
+import co.edu.uniandes.csw.requisitos.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.requisitos.persistence.EquipoDesarrolloPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author Juan Martinez
+ */
+@RunWith(Arquillian.class)
+public class EquipoDesarrolloLogicTest {
+    private PodamFactory factory = new PodamFactoryImpl();
+    
+    @Inject
+    private EquipoDesarrolloLogic equipoLogic;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+     @Inject
+    UserTransaction utx;
+    
+    /**
+     * Lista de ProyectoEntitys creados por Podam
+     */
+    private final List<EquipoDesarrolloEntity> list = new ArrayList<>();
+    
+    @Deployment
+    public static JavaArchive createDeployment(){
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(EquipoDesarrolloEntity.class.getPackage())
+                .addPackage(EquipoDesarrolloLogic.class.getPackage())
+                .addPackage(EquipoDesarrolloPersistence.class.getPackage())
+                .addAsManifestResource("Meta-INF/persistence.xml","persistence.xml")
+                .addAsManifestResource("Meta-INF/beans.xml","beans.xml");
+                
+    }
+    
+     /**
+     * Configuración inicial de todas las pruebas.
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     */
+    private void clearData() {
+        em.createQuery("delete from ProyectoEntity").executeUpdate();
+    }
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            EquipoDesarrolloEntity entidad = factory.manufacturePojo(EquipoDesarrolloEntity.class);
+            em.persist(entidad);
+            list.add(entidad);
+        }
+    }
+    
+    /**
+     * Prueba la creacion de un EquipoDesarrollo
+     * @throws BusinessLogicException 
+     */
+    @Test
+    public void createProyecto() throws BusinessLogicException{
+        EquipoDesarrolloEntity entidadNueva = factory.manufacturePojo(EquipoDesarrolloEntity.class);
+        EquipoDesarrolloEntity result = equipoLogic.createEquipoDesarrollo(entidadNueva);
+        Assert.assertNotNull(result);
+        
+        EquipoDesarrolloEntity entidad = em.find(EquipoDesarrolloEntity.class, result.getId());
+        Assert.assertEquals(entidad.getId(), result.getId());
+    }
+    
+    
+    /**
+     * Prueba para get un EquipoDesarrollo
+     */
+    @Test
+    public void getEquipoTest() {
+        EquipoDesarrolloEntity nuevaEnt = list.get(0);
+        EquipoDesarrolloEntity entidad = equipoLogic.getEquipo(nuevaEnt.getId());
+        Assert.assertNotNull(entidad);
+        
+        Assert.assertEquals(nuevaEnt.getId(),entidad.getId()); 
+    }
+    
+    
+}
