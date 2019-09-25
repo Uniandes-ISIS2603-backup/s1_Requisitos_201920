1c64efb9ec1e2a5fafad4da7cfbaaba7396d93a5
rodrigo0097 <rj.gonzalez10@uniandes.edu.co>
2019-09-25 13:56:48 -0500

    Test de lógica de representante funcionando, dto de iteración acabada y casi acabados los dto de representante

---
+++ s1_requisitos-api/src/main/java/co/edu/uniandes/csw/requisitos/dtos/IteracionDTO.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requisitos.dtos;
+
+import co.edu.uniandes.csw.requisitos.entities.IteracionEntity;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author rj.gonzalez10
+ */
+public class IteracionDTO implements Serializable {
+    //numbre de la iteración
+    private String nombre;
+    //descripción de la iteración
+    private String descripcion;
+    //fecha de inicio de la iteración
+    private Date fechaInicio;
+   //fecha de fin de la iteración 
+    private Date fechaFin;
+    //identificador de la iteracion
+    private Long id;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public IteracionEntity toEntity()
+    { IteracionEntity entidad = new IteracionEntity();
+      entidad.setDescripcion(descripcion);
+      entidad.setId(id);
+      entidad.setFechaInicio(fechaInicio);
+      entidad.setFechaFin(fechaFin);
+      entidad.setNombre(nombre);
+      return entidad;     
+    }
+    public IteracionDTO(IteracionEntity pIteracionEntity)
+    {
+        this.setDescripcion(pIteracionEntity.getDescripcion());
+        this.setFechaFin(pIteracionEntity.getFechaFin());
+        this.setFechaInicio(pIteracionEntity.getFechaInicio());
+        this.setId(pIteracionEntity.getId());
+        this.setNombre(pIteracionEntity.getNombre());
+    }
+    public IteracionDTO(){
+        
+    }
+}
---
+++ s1_requisitos-api/src/main/java/co/edu/uniandes/csw/requisitos/dtos/RepresentanteDelClienteDTO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requisitos.dtos;
+
+import co.edu.uniandes.csw.requisitos.entities.RepresentanteDelClienteEntity;
+import java.io.Serializable;
+
+/**
+ *
+ * @author rj.gonzalez10
+ */
+public class RepresentanteDelClienteDTO implements Serializable {
+
+    private String nombre;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public RepresentanteDelClienteEntity toEntity()
+    {
+        RepresentanteDelClienteEntity representante = new RepresentanteDelClienteEntity();
+        representante.setNombre(nombre);
+        return representante;
+    }
+    
+    public RepresentanteDelClienteDTO(RepresentanteDelClienteEntity representante)
+    {
+        this.setNombre(representante.getNombre());
+    }
+
+    
+}
