2bf1a8b994f1f2adf88fac30e72dac8e5f3eb10e
rodrigo0097 <rj.gonzalez10@uniandes.edu.co>
2019-10-08 15:12:03 -0500

    Adaptando representanteDelCliente a su asociación con casos de uso

--- s1_requisitos-api/src/main/java/co/edu/uniandes/csw/requisitos/dtos/RepresentanteDelClienteDetailDTO.java
+++ s1_requisitos-api/src/main/java/co/edu/uniandes/csw/requisitos/dtos/RepresentanteDelClienteDetailDTO.java
@@ -5,21 +5,59 @@
  */
 package co.edu.uniandes.csw.requisitos.dtos;
 
+import co.edu.uniandes.csw.requisitos.entities.CasoDeUsoEntity;
 import co.edu.uniandes.csw.requisitos.entities.RepresentanteDelClienteEntity;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author rj.gonzalez10
  */
 public class RepresentanteDelClienteDetailDTO extends RepresentanteDelClienteDTO {
+    private List<CasoDeUsoDTO> casosDeUso;
     
     public RepresentanteDelClienteDetailDTO(RepresentanteDelClienteEntity entidad)
     {
         super(entidad);
+         if (entidad != null) {
+            if (entidad.getCasosDeUso() != null) {
+                casosDeUso = new ArrayList<>();
+                for (CasoDeUsoEntity entityBook : entidad.getCasosDeUso()) {
+                    casosDeUso.add(new CasoDeUsoDTO(entityBook));
+                }
+            }
+        }
     }
      public RepresentanteDelClienteDetailDTO()
     {
         
+    }
+      @Override
+    public RepresentanteDelClienteEntity toEntity() {
+        RepresentanteDelClienteEntity representante = super.toEntity();
+        if (casosDeUso != null) {
+            List<CasoDeUsoEntity> casosEntity = new ArrayList<>();
+            for (CasoDeUsoDTO dtoCaso : casosDeUso) {
+                casosEntity.add(dtoCaso.toEntity());
+            }
+            representante.setCasosDeUso(casosEntity);
+        }
+        return representante;
+    }
+
+    /**
+     * @return the casosDeUso
+     */
+    public List<CasoDeUsoDTO> getCasosDeUso() {
+        return casosDeUso;
+    }
+
+    /**
+     * @param casosDeUso the casosDeUso to set
+     */
+    public void setCasosDeUso(List<CasoDeUsoDTO> casosDeUso) {
+        this.casosDeUso = casosDeUso;
     }
     
 }
