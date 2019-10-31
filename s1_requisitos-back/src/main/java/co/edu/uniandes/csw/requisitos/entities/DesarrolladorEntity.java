e3b69341620dd4837cd20cb2f1e9c37e9e326db0
Nicole Bahamon <n.bahamon@uniandes.edu.co>
2019-10-31 05:12:58 -0500

    Algunos cambios Desarrollador, falta aún varias

--- s1_requisitos-back/src/main/java/co/edu/uniandes/csw/requisitos/entities/DesarrolladorEntity.java
+++ s1_requisitos-back/src/main/java/co/edu/uniandes/csw/requisitos/entities/DesarrolladorEntity.java
@@ -26,6 +26,10 @@ public class DesarrolladorEntity extends BaseEntity implements Serializable {
      * String que representa el tipo del desarrollador
      */
     private String tipo;
+    private String nombre;
+    private String correo;
+    private Integer edad;
+    private Integer cedula;
     /**
      * Lista de requisitos que tiene un desarrollador
      */
@@ -122,6 +126,62 @@ public class DesarrolladorEntity extends BaseEntity implements Serializable {
      */
     public void setModificaciones(List <ModificacionesEntity> modificaciones) {
         this.modificaciones = modificaciones;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the edad
+     */
+    public Integer getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public Integer getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(Integer cedula) {
+        this.cedula = cedula;
     }
 
    
